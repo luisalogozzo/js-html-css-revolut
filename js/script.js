b74b432bff217806d371d716e096488b9cb87408
Luisa Logozzo <luisa.logozzo@gmail.com>
2020-01-20 22:38:52 +0100

    up

--- js/script.js
+++ js/script.js
@@ -1,27 +1,19 @@
 $(document).ready(function() {
-$('.header-nav .dropdown-parent').hover(function() {
-    $('.dropdown').removeClass("display-none");
-   }
-   // function() {
-  //   $('.dropdown').addClass("display-none");
-  // }
 
+  $('.dropdown-parent').hover(function(){
+    $('.dropdown').hide();
+    $(this).children('ul').show();
+    // $(this).siblings().children('ul').hide();
+  });
 
+  $('ul li').hover(function(){
+    $('ul li a').removeClass('black');
+    $(this).children('a').addClass('black');
+  });
 
-);
-
-
-
-
-
-
-
-
-
-
-
-
-
+  $(document).click( function(event){
+    $(".dropdown").hide();
+  });
 
 
 });
